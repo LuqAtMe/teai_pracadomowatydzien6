1bbe4df075ea1f8edf22805c93a0f5efb1421c95
brzeznicki.lukasz@gmail.com <Gryfek69>
2020-03-15 14:24:33 +0100

    Initial commit

---
+++ src/main/java/home/application/teai_pracadomowatydzien6/TeaiPracadomowatydzien6Application.java
@@ -0,0 +1,13 @@
+package home.application.teai_pracadomowatydzien6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaiPracadomowatydzien6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaiPracadomowatydzien6Application.class, args);
+    }
+
+}
---
+++ src/test/java/home/application/teai_pracadomowatydzien6/TeaiPracadomowatydzien6ApplicationTests.java
@@ -0,0 +1,13 @@
+package home.application.teai_pracadomowatydzien6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaiPracadomowatydzien6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
